14fea0dac900b7295e9b3d1a879fa0df01eda7e8
ogatore <ogata@ogatore.jp>
2026-07-06 01:16:22 +0900

    白抜きカード内をぜんぶバナナスリップに: かな+カード用漢字の27KBサブセット(未収録の柔坊超はM PLUSへフォールバック)・タイプ行38px/ありがとう行32px・フォントURLにv2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // #きょうのオガトレ オフラインキャッシュ（https配信時のみ有効）
-const C="kyono-v4";
-const ASSETS=["./","index.html","videos.js","manifest.json","assets/chara.png","assets/chara-good.png","assets/chara-kaikyaku.png","assets/icon-192.png","assets/icon-512.png","assets/icon-180.png","assets/fonts/banana-num.woff2"];
+const C="kyono-v5";
+const ASSETS=["./","index.html","videos.js","manifest.json","assets/chara.png","assets/chara-good.png","assets/chara-kaikyaku.png","assets/icon-192.png","assets/icon-512.png","assets/icon-180.png","assets/fonts/banana-card.woff2"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{
